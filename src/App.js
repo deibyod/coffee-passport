6baef54dac4b304ff47f9e51249b06be8590128b
DeibyOd <deibyod@gmail.com>
2025-02-07 19:01:53 -0300

    Fix Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Story from './components/Story/Story';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Footer from './components/Footer/Footer';
@@ -7,20 +7,18 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Coffee Passport</h1>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Story />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Coffee Passport</h1>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Story />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
