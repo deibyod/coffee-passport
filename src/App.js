82b26bd0d4169ea0edc9aad9e850987d76224e6b
DeibyOd <deibyod@gmail.com>
2025-02-07 16:15:25 -0300

    Change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Story from './components/Story/Story';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Footer from './components/Footer/Footer';
